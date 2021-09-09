ec869d0b7cd8c37ef101be30534678e8b8e9129e
biancaluzz <88661727+biancaluzz@users.noreply.github.com>
2021-09-09 11:44:32 -0300

    Update PersonTests.cs

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -13,9 +13,38 @@ namespace Tests
         }
 
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void TestValidId() // Cambiá el nombre para indicar qué estás probando
         {
-            // Insertá tu código  de pruebaaquí
+            // Insertá tu código  de prueba aquí
+            Person person = new Person("Claudia Perez", "5.378.614-6");
+            person.ID = "2.004.449-6";
+            Assert.AreEqual("2.004.449-6", person.ID);
         }
+        [Test]
+        public void TestInvalidId() // Cambiá el nombre para indicar qué estás probando
+        {
+            // Insertá tu código  de prueba aquí
+            Person person = new Person("Claudia Perez", "5.378.614-6");
+            person.ID = "5.378.614-5";
+            Assert.AreEqual("5.378.614-6", person.ID);
+        }
+        [Test]
+        public void TestValidName() // Cambiá el nombre para indicar qué estás probando
+        {
+            // Insertá tu código  de prueba aquí
+            Person person = new Person("Claudia Perez", "6.834.009-2");
+            person.Name = "Fernanda Olivera";
+            Assert.AreEqual("Fernanda Olivera", person.Name);
+        }
+        [Test]
+        public void TestInvalidName() // Cambiá el nombre para indicar qué estás probando
+        {
+            // Insertá tu código  de prueba aquí
+            Person person = new Person("Claudia Perez", "6.834.009-2");
+            person.Name = "";
+            Assert.AreEqual("Claudia Perez", person.Name);
+        }
+        
+        
     }
 }
